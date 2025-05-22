dc674bce8ad035b4f5cc43906e70ae3c576db0e2
Murielton Maximiano <muryfarben@gmail.com>
2025-05-22 19:30:00 -0300

    criado Exercicio07

---
+++ src/main/java/com/Exercicios07.java
@@ -0,0 +1,21 @@
+package main.java.com;
+
+import java.util.Scanner;
+
+public class Exercicios07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro número");
+        int primeiroNumero = scanner.nextInt();
+        System.out.println("Digite o segundo número");
+        int segundoNumero = scanner.nextInt();
+
+        int soma = primeiroNumero + segundoNumero;
+        int subtracao = primeiroNumero - segundoNumero;
+        int multiplicacao = primeiroNumero * segundoNumero;
+        double divisao = primeiroNumero / (double)segundoNumero;
+
+        System.out.println("Soma: " + soma + "subtração:" + subtracao
+        + "Multiplicação: " + multiplicacao + "Divisão:" + divisao);
+    }
+}
